bd2d34cebeb084816227479ca90143185d7803f1
guzhongren <guzhongren@live.cn>
2021-03-13 23:03:03 +0800

    pref: fomat with editorconfig

--- src/core.js
+++ src/core.js
@@ -3,37 +3,43 @@ let totalCount = 0;
 let testCaseResults = [];
 
 const test = (name, callback) => {
-    totalCount++;
-    try {
-        callback();
-        testCaseResults.push({name, isPassed: true});
-    } catch (e) {
-        testCaseResults.push({name, isPassed: false});
-        throw e;
-    }
+  totalCount++;
+  try {
+    callback();
+    testCaseResults.push({ name, isPassed: true });
+  } catch (e) {
+    testCaseResults.push({ name, isPassed: false });
+    throw e;
+  }
 };
 
-const getToBe = actual => (expected) => {
-    if (actual !== expected) {
-        throw new Error("");
-    }
-    passedCount++;
+const getToBe = (actual) => (expected) => {
+  if (actual !== expected) {
+    throw new Error("");
+  }
+  passedCount++;
 };
 
 const expect = (actual) => {
-    return {toBe: getToBe(actual)};
+  return { toBe: getToBe(actual) };
 };
 
 const getPassedCount = () => {
-    return passedCount;
+  return passedCount;
 };
 
 const getTotalCount = () => {
-    return totalCount;
+  return totalCount;
 };
 
 const getTestCaseResults = () => {
-    return testCaseResults;
+  return testCaseResults;
 };
 
-module.exports = {test, expect, getPassedCount, getTotalCount, getTestCaseResults};
+module.exports = {
+  test,
+  expect,
+  getPassedCount,
+  getTotalCount,
+  getTestCaseResults,
+};
--- src/hlj.js
+++ src/hlj.js
@@ -1,5 +1,11 @@
 const fileName = process.argv[2];
-const {test, expect, getPassedCount, getTotalCount, getTestCaseResults} = require('./core');
+const {
+  test,
+  expect,
+  getPassedCount,
+  getTotalCount,
+  getTestCaseResults,
+} = require("./core");
 
 const runTest = (fileName) => {
   global.test = test;
@@ -7,8 +13,10 @@ const runTest = (fileName) => {
   require("../" + fileName);
 };
 
-const formatTestResult = testCaseResults =>
-    testCaseResults.map(testCase => `  ${testCase.isPassed ? '✓' : '✕'} ${testCase.name}`).join("\n");
+const formatTestResult = (testCaseResults) =>
+  testCaseResults
+    .map((testCase) => `  ${testCase.isPassed ? "✓" : "✕"} ${testCase.name}`)
+    .join("\n");
 
 const getFailedCountString = () => {
   const failedCount = getTotalCount() - getPassedCount();
@@ -20,16 +28,16 @@ const getFailedCountString = () => {
 };
 
 let executionTime = 0;
-const getExecutionTime = () => `Time: ${(executionTime / 1000)} s`;
+const getExecutionTime = () => `Time: ${executionTime / 1000} s`;
 
-const getTestResult = isPassed => {
-  let output = ''
-  output += isPassed ? 'PASS' : 'FAIL'
-  output += ` ${fileName}\n`
-  output += `${formatTestResult(getTestCaseResults())}\n`
+const getTestResult = (isPassed) => {
+  let output = "";
+  output += isPassed ? "PASS" : "FAIL";
+  output += ` ${fileName}\n`;
+  output += `${formatTestResult(getTestCaseResults())}\n`;
   output += `Tests: `;
   output += getFailedCountString();
-  output += `${(getPassedCount())} passed, ${getTotalCount()} total\n`;
+  output += `${getPassedCount()} passed, ${getTotalCount()} total\n`;
   output += getExecutionTime();
   return output;
 };
@@ -43,5 +51,5 @@ try {
   isPassed = false;
 } finally {
   executionTime = Date.now() - startAt;
-  console.log(getTestResult(isPassed))
+  console.log(getTestResult(isPassed));
 }
