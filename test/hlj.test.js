3858ef6ae2bca1e41b80b603b10e45f814fd8cb6
Seaborn Lee <seabornlee.cn@gmail.com>
2021-03-13 21:13:49 +0800

    refactor: rename jtdd to hlj

--- test/hlj.test.js
+++ test/hlj.test.js
@@ -12,26 +12,26 @@ const exec = (cmd) => {
 
 describe("JavaScript TDD framework", () => {
   it("is a CLI program", () => {
-    const stdout = exec("jtdd sum.test.js");
+    const stdout = exec("hlj sum.test.js");
     expect(stdout).toContain("PASS");
   });
   it("should return PASS when test passed", () => {
-    const stdout = exec("jtdd sum.test.js");
+    const stdout = exec("hlj sum.test.js");
     expect(stdout).toContain("PASS");
   });
   it('should return ERROR when test failed', () => {
-    const stdout = exec("jtdd failed.test.js");
+    const stdout = exec("hlj failed.test.js");
     expect(stdout).toContain("ERROR");
   });
   describe('Test report', () => {
     it('should output number of tests passed', () => {
-      const stdout = exec("jtdd two-tests.test.js");
+      const stdout = exec("hlj two-tests.test.js");
       expect(stdout).toBe("" +
           "PASS two-tests.test.js\n" +
           "Tests: 2 passed, 2 total\n");
     });
     it('should output number of tests passed while there are failed tests', () => {
-      const stdout = exec("jtdd passed-and-failed.test.js");
+      const stdout = exec("hlj passed-and-failed.test.js");
       expect(stdout).toBe("" +
           "ERROR passed-and-failed.test.js\n" +
           "Tests: 1 passed, 2 total\n");
