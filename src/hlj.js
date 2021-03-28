e16ee81ae4f721d6f116e58bd6bcb75612297630
Seaborn Lee <seabornlee.cn@gmail.com>
2021-03-29 00:08:50 +0800

    refactor: extract method

--- src/hlj.js
+++ src/hlj.js
@@ -3,13 +3,17 @@ const Parser = require('./parser/parser');
 const Walker = require('./walker');
 const ConsoleReporter = require('./report/consoleReporter');
 
+const main = (fileName, testCaseName) => {
+  const files = new Walker().walk(fileName);
+  const testReport = new Parser().parse(files);
+
+  testReport.execute(testCaseName);
+
+  const result = new ConsoleReporter(process.cwd() + '/', testReport).render();
+  console.log(result);
+};
+
 const fileName = process.cwd() + '/' + process.argv[2];
 const testCaseName = process.argv[3];
 
-const files = new Walker().walk(fileName);
-const testReport = new Parser().parse(files);
-
-testReport.execute(testCaseName);
-
-const result = new ConsoleReporter(process.cwd() + '/', testReport).render();
-console.log(result);
+main(fileName, testCaseName);
