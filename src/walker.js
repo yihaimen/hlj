613e8cf438ad13d1d34d13b1ea06e1eb3a706301
Seaborn Lee <seabornlee.cn@gmail.com>
2021-03-28 11:27:53 +0800

    feat: read test files

---
+++ src/walker.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+class Walker {
+  constructor() {
+    this.files = [];
+  }
+
+  walk(path) {
+    if (this.isTestFile(path)) {
+      this.files.push(path);
+      return;
+    }
+
+    if (this.isDir(path)) {
+      const fileNames = fs.readdirSync(path);
+      fileNames.forEach((fileName) => {
+        this.walk(path + '/' + fileName);
+      });
+    }
+    return this.files;
+  }
+
+  isDir(fileName) {
+    return fs.lstatSync(fileName).isDirectory();
+  }
+
+  isTestFile(fileName) {
+    return fileName.endsWith('.test.js');
+  }
+}
+
+module.exports = Walker;
